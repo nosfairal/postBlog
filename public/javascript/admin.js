0be1f9b7bbd6c27b26b849cbe665b89d608ae80d
nosfair <nosfairalchtion@gmail.com>
2022-02-09 10:02:40 +0100

    correction of CSS and JS

--- public/javascript/admin.js
+++ public/javascript/admin.js
@@ -1,14 +1,16 @@
+function approuve(){
+    let xmlhttp = new XMLHttpRequest;
+    xmlhttp.open("GET", "https://localhost/blogpost/index.php?p=admin/approuveComment/"+this.dataset.id);
+    xmlhttp.send();
+};
+
 window.onload = () => {
     let buttons = document.querySelectorAll(".form-check-input");
     for(let button of buttons){
         button.addEventListener("click", approuve);
     }
-};
-
-function approuve(){
-    let xmlhttp = new XMLHttpRequest;
-    xmlhttp.open('GET', "https://localhost/blogpost/index.php?p=admin/approuveComment/"+this.dataset.id);
-    xmlhttp.send();
 }
+
+
 
 
--- public/javascript/adminUser.js
+++ public/javascript/adminUser.js
@@ -1,28 +1,33 @@
-window.onload = () => {init1();init2()}
+function approuveUser(){
+    let xmlhttp = new XMLHttpRequest;
+    xmlhttp.open("GET", "https://localhost/blogpost/index.php?p=admin/approuveUser/"+this.dataset.id);
+    xmlhttp.send();
+};
 
+function upgradeUser(){
+    let xmlhttp2 = new XMLHttpRequest;
+    xmlhttp2.open("GET", "https://localhost/blogpost/index.php?p=admin/upgradeUser/"+this.dataset.id);
+    xmlhttp2.send();
+};
 
 function init1(){
     let buttonsUsers = document.querySelectorAll(".form-check-input");
     for(let buttonsUser of buttonsUsers){
-        buttonsUser.addEventListener("click", approuveUser)
+        buttonsUser.addEventListener("click", approuveUser);
     }
-}
+};
+
 function init2(){    
     let upgradeButtons = document.querySelectorAll(".btn-success");
     for(let  upgradeButton of upgradeButtons){
         upgradeButton.addEventListener("click", upgradeUser);
     }
-}
+};
 
-function approuveUser(){
-    let xmlhttp = new XMLHttpRequest;
-    xmlhttp.open('GET', 'https://localhost/blogpost/index.php?p=admin/approuveUser/'+this.dataset.id);
-    xmlhttp.send();
-}
+window.onload = () => {init1();init2()}
 
-function upgradeUser(){
-    let xmlhttp2 = new XMLHttpRequest;
-    xmlhttp2.open('GET', 'https://localhost/blogpost/index.php?p=admin/upgradeUser/'+this.dataset.id);
-    xmlhttp2.send();
-}
+
+
+
+
 
