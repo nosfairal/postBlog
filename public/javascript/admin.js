ae59a2b7d36887b1321d18a33ca20339536aadbe
nosfair <nosfairalchtion@gmail.com>
2022-02-09 10:14:03 +0100

    correcting CSS and JS missed

--- public/javascript/admin.js
+++ public/javascript/admin.js
@@ -2,7 +2,7 @@ function approuve(){
     let xmlhttp = new XMLHttpRequest;
     xmlhttp.open("GET", "https://localhost/blogpost/index.php?p=admin/approuveComment/"+this.dataset.id);
     xmlhttp.send();
-};
+}
 
 window.onload = () => {
     let buttons = document.querySelectorAll(".form-check-input");
--- public/javascript/adminUser.js
+++ public/javascript/adminUser.js
@@ -2,27 +2,27 @@ function approuveUser(){
     let xmlhttp = new XMLHttpRequest;
     xmlhttp.open("GET", "https://localhost/blogpost/index.php?p=admin/approuveUser/"+this.dataset.id);
     xmlhttp.send();
-};
+}
 
 function upgradeUser(){
     let xmlhttp2 = new XMLHttpRequest;
     xmlhttp2.open("GET", "https://localhost/blogpost/index.php?p=admin/upgradeUser/"+this.dataset.id);
     xmlhttp2.send();
-};
+}
 
 function init1(){
     let buttonsUsers = document.querySelectorAll(".form-check-input");
     for(let buttonsUser of buttonsUsers){
         buttonsUser.addEventListener("click", approuveUser);
     }
-};
+}
 
 function init2(){    
     let upgradeButtons = document.querySelectorAll(".btn-success");
     for(let  upgradeButton of upgradeButtons){
         upgradeButton.addEventListener("click", upgradeUser);
     }
-};
+}
 
 window.onload = () => {init1();init2()}
 
